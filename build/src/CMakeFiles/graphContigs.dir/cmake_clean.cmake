97f23193f115f398a4969afb95ce97c35975a3ab
lanatuk <lana.tukovic@gmail.com>
2023-04-13 23:47:52 +0200

    Added overlaps files.

---
+++ build/src/CMakeFiles/graphContigs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphContigs.dir/main.cpp.o"
+  "CMakeFiles/graphContigs.dir/main.cpp.o.d"
+  "graphContigs"
+  "graphContigs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphContigs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/graphContigs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for graphContigs.
+# This may be replaced when dependencies are built.
